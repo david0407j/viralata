20ce8cfbd287d604aa3c77877ac4cdd55eb02c0d
Davidson <deivisonj1@hotmail.com>
2024-09-04 22:38:22 -0300

    pytest test da home

--- viralata/base/models.py
+++ viralata/base/models.py
@@ -20,12 +20,12 @@ class Categoria(models.Model):
 
 
 class Foto(models.Model):
-    name = models.CharField(max_length=100)
-    image = models.ImageField(upload_to="fotos/")
-    description = models.TextField(blank=True)
     categoria = models.ForeignKey(
         Categoria, on_delete=models.CASCADE, related_name="fotos"
     )
+    name = models.CharField(max_length=100)
+    image = models.ImageField(upload_to="fotos/")
+    description = models.TextField(blank=True)
 
     def __str__(self):
         return self.name
